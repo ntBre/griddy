e945fd54c53de7a44c44a0e2360cfea0403936de
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-24 22:54:11 -0400

    copy over Cart::first_part, add symm

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,131 @@
+use std::error::Error;
 use std::io::stdout;
 use std::path::Path;
 
 use pbqff::config::Config;
 use pbqff::coord_type::cart::{freqs, FirstOutput};
+use pbqff::coord_type::findiff::bighash::BigHash;
 use pbqff::coord_type::findiff::FiniteDifference;
 use pbqff::coord_type::{Cart, Derivative, FirstPart, Nderiv};
-use pbqff::{cleanup, Output, Spectro};
+use pbqff::{cleanup, optimize, ref_energy, Output, Spectro};
+use psqs::geom::Geom;
 use psqs::max_threads;
 use psqs::program::molpro::Molpro;
+use psqs::program::{Job, Program, Template};
 use psqs::queue::pbs::Pbs;
+use psqs::queue::{Check, Queue};
+use symm::Molecule;
+
+fn first_part(
+    config: &FirstPart,
+    queue: &Pbs,
+    nderiv: Nderiv,
+    root_dir: impl AsRef<Path>,
+    pts_dir: impl AsRef<Path>,
+) -> Result<FirstOutput, Box<dyn Error>> {
+    let template = Template::from(&config.template);
+    let (geom, ref_energy) = if config.optimize {
+        let res = optimize::<_, Molpro>(
+            &root_dir,
+            queue,
+            config.geometry.clone(),
+            template.clone(),
+            config.charge,
+        )
+        .expect("optimization failed");
+        let Some(cart) = res.cart_geom else {
+            panic!("failed to extract cart geom from {res:?}");
+        };
+        (Geom::Xyz(cart), res.energy)
+    } else {
+        let ref_energy = ref_energy::<_, Molpro>(
+            queue,
+            config.geometry.clone(),
+            template.clone(),
+            config.charge,
+        );
+        (config.geometry.clone(), ref_energy)
+    };
+    let geom = geom.xyz().expect("expected an XYZ geometry, not Zmat");
+    let ndummies = config.dummy_atoms.unwrap_or(0);
+    // 3 * (#atoms - #dummy_atoms)
+    let n = 3 * (geom.len() - ndummies);
+    let nfc2 = n * n;
+    let nfc3 = n * (n + 1) * (n + 2) / 6;
+    let nfc4 = n * (n + 1) * (n + 2) * (n + 3) / 24;
+    let deriv = match nderiv {
+        Nderiv::Two => Derivative::Harmonic(nfc2),
+        Nderiv::Four => Derivative::Quartic(nfc2, nfc3, nfc4),
+    };
+    let mut fcs = vec![0.0; nfc2 + nfc3 + nfc4];
+    let mut mol = Molecule::new(geom.to_vec());
+    if let Some(ws) = &config.weights {
+        for (i, w) in ws.iter().enumerate() {
+            mol.atoms[i].weight = Some(*w);
+        }
+    }
+    mol.normalize();
+    let pg = mol.point_group();
+    println!("normalized geometry:\n{mol}");
+    println!("point group:{pg}");
+    let mut target_map = BigHash::new(mol.clone(), pg);
+    let geoms = Cart.build_points(
+        Geom::Xyz(mol.atoms.clone()),
+        config.step_size,
+        ref_energy,
+        deriv,
+        &mut fcs,
+        &mut target_map,
+        n,
+    );
+    let targets = target_map.values();
+    let jobs: Vec<_> = geoms
+        .into_iter()
+        .enumerate()
+        .map(|(job_num, mol)| {
+            let filename = format!("job.{job_num:08}");
+            let filename = pts_dir
+                .as_ref()
+                .join(filename)
+                .to_string_lossy()
+                .to_string();
+            Job::new(
+                Molpro::new(
+                    filename,
+                    template.clone(),
+                    config.charge,
+                    mol.geom,
+                ),
+                mol.index,
+            )
+        })
+        .collect();
+    let njobs = jobs.len();
+    println!("{n} Cartesian coordinates requires {njobs} points");
+
+    // drain into energies
+    let mut energies = vec![0.0; njobs];
+    let time = queue.drain(
+        pts_dir.as_ref().to_str().unwrap(),
+        jobs,
+        &mut energies,
+        Check::None,
+    )?;
+
+    eprintln!("total job time: {time:.1} sec");
+
+    Ok(FirstOutput {
+        n,
+        nfc2,
+        nfc3,
+        fcs,
+        mol,
+        energies,
+        targets,
+        ref_energy,
+        pg,
+    })
+}
 
 fn run(
     dir: impl AsRef<Path>,
@@ -24,16 +141,14 @@ fn run(
         energies,
         targets,
         ..
-    } = Cart
-        .first_part::<_, _, Molpro>(
-            &mut stdout(),
-            &FirstPart::from(config.clone()),
-            queue,
-            Nderiv::Four,
-            &dir,
-            dir.as_ref().join("pts"),
-        )
-        .unwrap();
+    } = first_part(
+        &FirstPart::from(config.clone()),
+        queue,
+        Nderiv::Four,
+        &dir,
+        dir.as_ref().join("pts"),
+    )
+    .unwrap();
 
     let freq_dir = &dir.as_ref().join("freqs");
     let (fc2, f3, f4) = Cart.make_fcs(
